14c685994fc83f8b8c9f87a776cc058128ead48a
unknown <shivam.aashir@gmail.com>
2018-09-22 16:06:35 -0700

    Message added

--- src/main/java/config/CloudConfig.java
+++ src/main/java/config/CloudConfig.java
@@ -3,10 +3,17 @@ package config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @SpringBootApplication
+@RestController
 public class CloudConfig {
+	@GetMapping (path ="cloud")
+	public String message() {
+		return "Not Authorized";
+	}
 	
 	public static void main (String[] args) {
 		SpringApplication.run(CloudConfig.class);
